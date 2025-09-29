eea9902f028955440b920d23a6762582403581ca
Daniel Argüeso <d.argueso@uib.es>
2025-09-29 11:28:26 +0200

    Added flexibility to periods
    Added flexibility to periods and experiments, info passed with config file

--- Calculate_CMIP6_Annual_cycle-CC_change-regrid_ERA5.py
+++ Calculate_CMIP6_Annual_cycle-CC_change-regrid_ERA5.py
@@ -24,9 +24,10 @@ import subprocess as subprocess
 from glob import glob
 import pandas as pd
 import xarray as xr
-import os, argparse
+import os
 from pathlib import Path
 from tqdm.auto import tqdm
+import pgw4era_config as cfg
 
 
 class bcolors:
@@ -44,92 +45,39 @@ class bcolors:
 #####################################################################
 #####################################################################
 
-
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description="PURPOSE: Create files with annual cycle of changes PGW"
-    )
-
-    parser.add_argument(
-        "-m",
-        "--models",
-        dest="models",
-        help="Optional input list of models",
-        type=str,
-        nargs="?",
-        default=None,
-    )
-
-    # variable(s) to process
-    parser.add_argument(
-        "-v",
-        "--var_names",
-        type=str,
-        help="Variable names (e.g. ta) to process. Separate "
-        + 'multiple variable names with "," (e.g. tas,ta). Default is '
-        + "to process all required variables hurs,tas,ps,ts,vas,uas,psl,ta,hur,ua,va,zg.",
-        default="hurs,tas,ps,ts,vas,uas,psl,ta,hur,ua,va,zg",
-    )
-
-    # year to start the calculation of the annual cycle
-    parser.add_argument(
-        "-s",
-        "--start_year",
-        type=int,
-        help="Year to start the calculation of the annual cycle",
-        default=1985,
-    )
+# Define a function to drop the 'bnds' dimension and associated variables
+def preprocess(ds):
+    # Drop variables associated with 'bnds' dimension
+    drop_vars = [var for var in ds.variables if 'bnds' in ds[var].dims]
+    ds = ds.drop_vars(drop_vars)
     
-    # year to end the calculation of the annual cycle
-    parser.add_argument(
-        "-e",
-        "--end_year",
-        type=int,
-        help="Year to end the calculation of the annual cycle",
-        default=2014,
-    )
-
-    # input directory
-    parser.add_argument(
-        "-i",
-        "--input_dir",
-        type=str,
-        help="Directory with input GCM delta files on ERA5 grid",
-        default="./",
-    )
-
-    # output directory
-    parser.add_argument(
-        "-o",
-        "--output_dir",
-        type=str,
-        help="Directory where the GCM ENSEMBLE delta files " + " should be stored.",
-        default="./",
-    )
+    # Drop the 'bnds' dimension itself if it exists
+    if 'bnds' in ds.dims:
+        ds = ds.drop_dims('bnds')
     
-
-    args = parser.parse_args()
-    return args
-
+    return ds
 
 #####################################################################
 #####################################################################
 
-args = parse_args()
-models_str = args.models
+models_str = cfg.models
 
 if models_str is None:
     with open("list_CMIP6.txt") as f:
         models = f.read().splitlines()
 else:
-    models = args.models.split(",")
+    models = cfg.models
 
-variables = args.var_names.split(",")
-idir = args.input_dir
-odir = args.output_dir
-experiments = ["historical", "ssp585"]
-syear_exp = {"historical": 1985, "ssp585": 2070}
-eyear_exp = {"historical": 2014, "ssp585": 2099}
+variables = cfg.variables_all
+experiments = cfg.experiments
+year_ranges = cfg.periods
+
+
+idir = cfg.CMIP6_monthly_dir
+odir = cfg.CMIP6anom_dir 
+
+# syear_exp = {"historical": 1985, "ssp585": 2015}
+# eyear_exp = {"historical": 2014, "ssp585": 2099}
 acycle_odir = f"{odir}/annual_cycle"
 deltas_odir = f"{odir}/deltas"
 regrid_era5 = f"{odir}/regrid_ERA5"
@@ -166,24 +114,32 @@ def main():
     Path(acycle_odir).mkdir(exist_ok=True, parents=True)
     Path(deltas_odir).mkdir(exist_ok=True, parents=True)
     Path(regrid_era5).mkdir(exist_ok=True, parents=True)
+    
+    print (f"{bcolors.OKGREEN}Processing periods: {year_ranges}{bcolors.ENDC}")
 
     for GCM in tqdm(models):
         # pbar.set_description(f"{GCM}")
 
         for vn, varname in enumerate(variables):
-            for exp in experiments:
-                syear = syear_exp[exp]
-                eyear = eyear_exp[exp]
+            
+            for period in year_ranges:
+                syear = period[0]
+                eyear = period[1]
+                print(f"{bcolors.OKCYAN}Processing period: {syear} - {eyear}{bcolors.ENDC}")
 
                 calculate_annual_cycle(
-                    GCM, varname, exp, syear, eyear, idir, acycle_odir
+                    GCM, varname, experiments, syear, eyear, idir, acycle_odir
                 )
 
-            calculate_CC_signal(GCM, varname, acycle_odir, deltas_odir)
+            calculate_CC_signal(GCM, varname, experiments, year_ranges, acycle_odir, deltas_odir)
 
             # REGRID TO ERA5
-            delta_file = f"{deltas_odir}/{GCM}/{varname}_delta.nc"
-            regrid_file = f"{regrid_era5}/{varname}_{GCM}_delta.nc"
+            syearp = year_ranges[0][0]
+            eyearp = year_ranges[0][1]
+            syearf = year_ranges[1][0]
+            eyearf = year_ranges[1][1]
+            delta_file = f"{deltas_odir}/{GCM}/{varname}_{syearp}-{eyearp}_{syearf}-{eyearf}_{'-'.join(experiments)}_delta.nc"
+            regrid_file = f"{regrid_era5}/{varname}_{syearp}-{eyearp}_{syearf}-{eyearf}_{'-'.join(experiments)}_{GCM}_delta.nc"
             if not os.path.isfile(regrid_file):
                 try:
                     subprocess.check_output(
@@ -201,22 +157,69 @@ def main():
 
 ###########################################################
 ###########################################################
-def calculate_annual_cycle(GCM, varname, exp, syear, eyear, idir, odir):
+def calculate_annual_cycle(GCM, varname, experiments, syear, eyear, idir, odir):
     """For a given model, member and variable,
     Calculate annual cycle"""
+    
+    filenames_p = sorted(glob(f"{idir}/{experiments[0]}/{varname}/{GCM}/{varname}*nc"))
+    finallp = xr.open_mfdataset(filenames_p, preprocess=preprocess)
+    if finallp.time.dtype == "O":
+        finallp["time"] = finallp["time"].astype("datetime64[ns]")
+    finallp_period = finallp.sel(time=slice(str(syear), str(eyear)))
 
-    filenames_all = sorted(glob(f"{idir}/{exp}/{varname}/{GCM}/{varname}*nc"))
-    finall = xr.open_mfdataset(filenames_all, concat_dim="time", combine="nested")
+    filenames_f = sorted(glob(f"{idir}/{experiments[1]}/{varname}/{GCM}/{varname}*nc"))
+    finallf = xr.open_mfdataset(filenames_f, preprocess=preprocess)
+    finallf = finallf.where(finallf[varname] != 0.)  
+    if finallf.time.dtype == "O":
+        finallf["time"] = finallf["time"].astype("datetime64[ns]")
+    finallf_period = finallf.sel(time=slice(str(syear), str(eyear)))
 
-    if finall.time.dtype == "O":
-        finall["time"] = finall["time"].astype("datetime64[ns]")
+    finall = xr.concat([finallp_period, finallf_period], dim="time")
 
+    # import pdb; pdb.set_trace()  # fmt: skip
+
+    # if syear < eyear_exp[experiments[0]] and eyear <= eyear_exp[experiments[0]]:
+    #     filenames_p = sorted(glob(f"{idir}/{experiments[0]}/{varname}/{GCM}/{varname}*nc"))
+    #     finall = xr.open_mfdataset(filenames_p, preprocess=preprocess)
+    # elif syear > syear_exp[experiments[1]] and eyear <= eyear_exp[experiments[1]]:
+    #     filenames_f = sorted(glob(f"{idir}/{experiments[1]}/{varname}/{GCM}/{varname}*nc"))
+    #     finall = xr.open_mfdataset(filenames_f, preprocess=preprocess)
+    # else:
+    #     raise SystemExit(
+    #         f"{bcolors.ERROR}ERROR: Requested years are not available: {GCM} {varname} {bcolors.ENDC}"
+    #     )
+    
+
+    # if finall_f.time.dtype == "O":
+    #     finall_f["time"] = finall_f["time"].astype("datetime64[ns]")
+    
+    #Concatenate the two datasets along the time dimension
+    #finall = xr.concat([finall_p, finall_f], dim="time")
+    
     Path(f"{odir}/{GCM}/").mkdir(exist_ok=True, parents=True)
 
-    ofname = f"{odir}/{GCM}/{varname}_{exp}.nc"
+    ofname = f"{odir}/{GCM}/{varname}_{syear}-{eyear}_{'-'.join(experiments)}.nc"
+    print (f"{bcolors.OKCYAN}Saving file: {ofname}{bcolors.ENDC}")
 
     if not os.path.isfile(ofname):
         fin_p = finall.sel(time=slice(str(syear), str(eyear)))
+        
+        # Convert the time variable to a pandas datetime series
+        avail_dates = pd.to_datetime(fin_p.time.values)
+
+        # Extract year and month as tuples (year, month)
+        year_months = set((date.year, date.month) for date in avail_dates)
+        
+        # Generate a set of all year-month combinations for the period
+        all_months = set((year, month) for year in range(syear,eyear+1) for month in range(1, 13))
+
+        # Check if the two sets are equal
+        all_months_present = year_months == all_months
+
+        if not all_months_present:
+            raise SystemExit(
+                f"{bcolors.ERROR}ERROR: Not all requested years/months seem to be available: {GCM} {varname} {bcolors.ENDC}"
+            )
 
         if varname == "hus":
             fin_p = fin_p.where((fin_p.hus >= 0) & (fin_p.hus <= 100))
@@ -231,40 +234,39 @@ def calculate_annual_cycle(GCM, varname, exp, syear, eyear, idir, odir):
         elif varname == "zg":
             fin_p = fin_p.where((fin_p.zg > -1000) & (fin_p.zg < 60000))
 
-        fin_p_mm = fin_p.groupby("time.month").mean("time")
+        fin_p_mm = fin_p.groupby("time.month").mean("time", skipna=False)
         fin_p_mm.to_netcdf(ofname)
         print(
-            f"{bcolors.OKGREEN}Created annual cycle file for {GCM} {varname} {exp}{bcolors.ENDC}"
+            f"{bcolors.OKGREEN}Created annual cycle file for {GCM} {varname} {syear}-{eyear}{bcolors.ENDC}"
         )
     else:
-        print(f"{bcolors.OKCYAN}{exp} {varname} {GCM} Already processed{bcolors.ENDC}")
+        print(f"{bcolors.OKCYAN}{varname} {GCM} {syear}-{eyear} Already processed{bcolors.ENDC}")
 
     finall.close()
-
+    
 
 ###########################################################
 ###########################################################
-def calculate_CC_signal(GCM, varname, idir, odir):
+def calculate_CC_signal(GCM, varname, experiments, year_ranges, idir, odir):
     """From present and future annual cycle
     calculate CC signal for every month"""
 
-    ofname = f"{odir}/{GCM}/{varname}_delta.nc"
+    syearp = year_ranges[0][0]
+    eyearp = year_ranges[0][1]
+    syearf = year_ranges[1][0]
+    eyearf = year_ranges[1][1]
+    
+
+
+    ofname = f"{odir}/{GCM}/{varname}_{syearp}-{eyearp}_{syearf}-{eyearf}_{'-'.join(experiments)}_delta.nc"
     Path(f"{odir}/{GCM}/").mkdir(exist_ok=True, parents=True)
 
     if not os.path.isfile(ofname):
-        fin_p = xr.open_dataset(f"{idir}/{GCM}/{varname}_historical.nc")
-        fin_f = xr.open_dataset(f"{idir}/{GCM}/{varname}_ssp585.nc")
-        # import pdb; pdb.set_trace()  # fmt: skip
+        fin_p = xr.open_dataset(f"{idir}/{GCM}/{varname}_{syearp}-{eyearp}_{'-'.join(experiments)}.nc")
+        fin_f = xr.open_dataset(f"{idir}/{GCM}/{varname}_{syearf}-{eyearf}_{'-'.join(experiments)}.nc")
         fin_d = fin_f - fin_p
 
-        if "plev_bnds" in fin_d.keys():
-            fin_d = fin_d.drop(("plev_bnds"))
-        if "lon_bnds" in fin_d.keys():
-            fin_d = fin_d.drop(("lon_bnds"))
-        if "lat_bnds" in fin_d.keys():
-            fin_d = fin_d.drop(("lat_bnds"))
-
-        datelist = pd.date_range(f"1990-01-01", periods=12, freq="MS")
+        datelist = pd.date_range(f"{syearp}-01-01", periods=12, freq="MS")
 
         foutclean = fin_d.rename({"month": "time"})
         foutclean = foutclean.assign_coords({"time": datelist})
@@ -281,6 +283,17 @@ def calculate_CC_signal(GCM, varname, idir, odir):
             f"{bcolors.OKCYAN}CC file {varname} {GCM} Already processed{bcolors.ENDC}"
         )
 
+def parse_year_periods(periods_string):
+    periods = periods_string.split(",")  # Split by comma to get each period
+    year_ranges = []
+
+    for period in periods:
+        start_year, end_year = period.split("-")  # Split each period by the hyphen
+        start_year = int(start_year.strip())  # Convert start year to integer
+        end_year = int(end_year.strip())  # Convert end year to integer
+        year_ranges.append((start_year, end_year))  # Append as a tuple
+
+    return year_ranges
 
 ###############################################################################
 ##### __main__  scope
